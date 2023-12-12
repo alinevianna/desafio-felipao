ae58a82ae2d5f9b646a19f43ed8f3bc588792eca
Aline Vianna <87501680+alinevianna@users.noreply.github.com>
2023-12-12 08:27:58 -0300

    conclusão nivelHeroi

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-let nome = "felipe";
-console.log(nome);
+let nomeHeroi = 'EletroChurras';
+let nivelHeroi = 9105;
+
+if (nivelHeroi <= 1000) {
+console.log('O Herói de nome '+nomeHeroi+' está no nível Ferro, com ' + nivelHeroi+ ' de XP.') }
+else if (nivelHeroi >= 1001 & nivelHeroi <= 2000) {
+    console.log('O Herói de nome '+nomeHeroi+' está no nível Bronze, com ' + nivelHeroi+ ' de XP.') }
+else if (nivelHeroi >= 2001 & nivelHeroi <= 5000) {
+    console.log('O Herói de nome '+nomeHeroi+' está no nível Prata, com ' + nivelHeroi+ ' de XP.') }
+else if (nivelHeroi >= 5001 & nivelHeroi <= 7000) {
+    console.log('O Herói de nome '+nomeHeroi+' está no nível Ouro, com ' + nivelHeroi+ ' de XP.') }
+else if (nivelHeroi >= 7001 & nivelHeroi <= 8000) {
+    console.log('O Herói de nome '+nomeHeroi+' está no nível Diamante, com ' + nivelHeroi+ ' de XP.') }
+else if (nivelHeroi >= 8001 & nivelHeroi <= 9000) {
+    console.log('O Herói de nome '+nomeHeroi+' está no nível Ascendente, com ' + nivelHeroi+ ' de XP.') }
+else if (nivelHeroi >= 9001 & nivelHeroi <= 10000) {
+    console.log('O Herói de nome '+nomeHeroi+' está no nível Imortal, com ' + nivelHeroi+ ' de XP.') }
+else {
+    console.log('O Herói de nome '+nomeHeroi+' está no nível Radiante, com ' + nivelHeroi+ ' de XP.') }
